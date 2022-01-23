e3a8032d122ced9bb1beab9c94e3a5cc8d769bd2
Zachary Sain <zachsain@gmail.com>
2022-01-23 14:17:23 -0600

    completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Article from "./Article"
 
 function ArticleList(props) {
+  
+
+    console.log("article list props", props)
     return (
         <main>
-            {props.map(prop => {
-                prop.key = prop;
-                return prop
-            })}
+           { props.posts.map(post => {
+               return <Article 
+               date = {post.date}
+               key = {post.id} 
+               minutes = {post.minutes}
+               preview = {post.preview}
+               title = {post.title} />
+           })}
         </main>
     )
 }
+
+
 
 export default ArticleList;
