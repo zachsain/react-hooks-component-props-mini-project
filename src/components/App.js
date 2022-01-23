a593ece8204343462d2c4c94e5f9e757efab65ca
Zachary Sain <zachsain@gmail.com>
2022-01-23 14:17:11 -0600

    commit tree 89f46ad4646b5a930113e7e5fe8311a875f49e0f

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
-console.log(blogData);
+console.log("blog Data", blogData);
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
       <About 
       about = {blogData.about}
       iamge = {blogData.image} />
-      <ArticleList />
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <ArticleList 
+      posts = {blogData.posts} 
+      />
     </div>
   );
 }
